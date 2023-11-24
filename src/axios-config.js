acac1935a66e8111008578b2db95792686a047a5
IsabelleHeloisa <isabelle.hello123@outlook.com>
2023-11-24 16:20:00 -0300

    feat: create memory form

---
+++ src/axios-config.js
@@ -0,0 +1,9 @@
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://localhost:3000/'
+
+axios.default.headers.post['Content-Type'] = 'application/json'
+
+axios.default.timeout = 10000
+
+export default axios
--- src/routes/AddMemory.jsx
+++ src/routes/AddMemory.jsx
@@ -1,7 +1,34 @@
 import React from 'react'
+import axios from 'axios'
+import './AddMemory.css'
+
+import { useState } from 'react'
 
 const AddMemory = () => {
-  return <div>AddMemory</div>
+  return (
+    <div className="add-memory-page">
+      <h2>Crie uma nova memória</h2>
+      <form>
+        <label>
+          <p>Título:</p>
+          <input type="text" placeholder="Defina um título" name="title" />
+        </label>
+        <label>
+          <p>Descrição:</p>
+          <textarea
+            type="text"
+            placeholder="Explique o que aconteceu..."
+            name="description"
+          ></textarea>
+        </label>
+        <label>
+          <p>Foto:</p>
+          <input type="file" name="image" />
+        </label>
+        <input type="submit" value="Enviar" className="btn" />
+      </form>
+    </div>
+  )
 }
 
 export default AddMemory
